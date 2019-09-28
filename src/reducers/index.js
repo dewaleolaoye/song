47bc03ba68baeaf1aad1aea985ff167d9924ce19
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-09-28 23:57:58 +0100

    configure connect with mapState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,5 +18,5 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
 export default combineReducers({
   songs: songsReducer,
-  selectedSong: selectedSongReducer
+  selectSong: selectedSongReducer
 });
