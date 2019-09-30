6f30f7b565158a6532442abdb504ccbee2958b56
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-09-30 23:56:47 +0100

    songDetail component with connect

---
+++ src/components/SongDetail.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = props => {
+  console.log(props);
+  return <div>Song Detail</div>;
+};
+
+const mapStateToProp = state => {
+  return { song: state.selectedSong };
+};
+
+export default connect(mapStateToProp)(SongDetail);
